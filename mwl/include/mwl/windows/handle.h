e01c1ea453448351c58e7fded2ac2f2f1e9dbb70
Park18 <whale.park18@gmail.com>
2026-04-06 23:30:41 +0900

    style (#4) apply .clang-format

--- mwl/include/mwl/windows/handle.h
+++ mwl/include/mwl/windows/handle.h
@@ -1,62 +1,73 @@
 #pragma once
-#include <memory>
-#include <concepts>
-#include <type_traits>
-
 #include <Windows.h>
 #include <spdlog/spdlog.h>
 
+#include <concepts>
+#include <memory>
+#include <type_traits>
+
 namespace mwl::windows
 {
-	using handle = HANDLE;
-	using hkey = HKEY;
 
-	template<class H>
-	concept handle_type = std::same_as<handle, H> || std::same_as<hkey, H>;
+using handle = HANDLE;
+using hkey = HKEY;
 
-	template<handle_type H>
-	struct handle_deleter
-	{
-		void operator()(H handle)
-		{
-			if constexpr (std::same_as<H, mwl::windows::handle>)
-			{
-				if (handle != nullptr)
-				{
-					spdlog::debug("Closed handle: {}", fmt::ptr(handle));
+template <class H>
+concept handle_type = std::same_as<handle, H> || std::same_as<hkey, H>;
 
-					::CloseHandle(handle);
-					handle = nullptr;
-				}
-			}
-			else if(std::same_as<H, mwl::windows::hkey>)
-			{
-				if (handle != nullptr)
-				{
-					spdlog::debug("Closed registry key handle: {}", fmt::ptr(handle));
+template <handle_type H>
+struct handle_deleter
+{
+    void operator()(H handle)
+    {
+        if constexpr (std::same_as<H, mwl::windows::handle>)
+        {
+            if (handle != nullptr)
+            {
+                spdlog::debug("Closed handle: {}", fmt::ptr(handle));
 
-					::RegCloseKey(handle);
-					handle = nullptr;
-				}
-			}
-		}
-	};
+                ::CloseHandle(handle);
+                handle = nullptr;
+            }
+        }
+        else if (std::same_as<H, mwl::windows::hkey>)
+        {
+            if (handle != nullptr)
+            {
+                spdlog::debug("Closed registry key handle: {}", fmt::ptr(handle));
 
-	template<handle_type H>
-	using unique_handle = std::unique_ptr<std::remove_pointer_t<H>, handle_deleter<H>>;
+                ::RegCloseKey(handle);
+                handle = nullptr;
+            }
+        }
+    }
+};
 
-	template<handle_type H>
-	using shared_handle = std::shared_ptr<std::remove_pointer_t<H>>;
+template <handle_type H>
+using unique_handle = std::unique_ptr<std::remove_pointer_t<H>, handle_deleter<H>>;
 
-	template<handle_type H>
-	using weak_handle = std::weak_ptr<std::remove_pointer_t<H>>;
+template <handle_type H>
+using shared_handle = std::shared_ptr<std::remove_pointer_t<H>>;
 
-	template<handle_type H>
-	inline unique_handle<H> make_unique_handle(H handle) { return unique_handle<H>(handle); }
+template <handle_type H>
+using weak_handle = std::weak_ptr<std::remove_pointer_t<H>>;
 
-	template<handle_type H>
-	inline shared_handle<H> make_shared_handle(H handle) { return shared_handle<H>(handle, handle_deleter<H>{}); }
-
-	template<handle_type H>
-	inline weak_handle<H> make_weak_handle(H shared_handle) { return weak_handle<H>(shared_handle); }
+template <handle_type H>
+inline unique_handle<H> make_unique_handle(H handle)
+{
+    return unique_handle<H>(handle);
 }
+
+template <handle_type H>
+inline shared_handle<H> make_shared_handle(H handle)
+{
+    return shared_handle<H>(handle, handle_deleter<H>{});
+}
+
+template <handle_type H>
+inline weak_handle<H> make_weak_handle(H shared_handle)
+{
+    return weak_handle<H>(shared_handle);
+}
+
+} // namespace mwl::windows
--- mwl/include/mwl/windows/reg_handler.h
+++ mwl/include/mwl/windows/reg_handler.h
@@ -3,22 +3,24 @@
 
 namespace mwl::windows
 {
-	enum class reg_root : ULONG_PTR
-	{
-		classes_root	= (ULONG_PTR)HKEY_CLASSES_ROOT,
-		current_config	= (ULONG_PTR)HKEY_CURRENT_CONFIG,
-		current_user	= (ULONG_PTR)HKEY_CURRENT_USER,
-		local_machine	= (ULONG_PTR)HKEY_LOCAL_MACHINE,
-		users			= (ULONG_PTR)HKEY_USERS,
-	};
 
-	template<reg_root R>
-	class reg_handler
-	{
-	public:
-		reg_handler() = default;
+enum class reg_root : ULONG_PTR
+{
+    classes_root = (ULONG_PTR)HKEY_CLASSES_ROOT,
+    current_config = (ULONG_PTR)HKEY_CURRENT_CONFIG,
+    current_user = (ULONG_PTR)HKEY_CURRENT_USER,
+    local_machine = (ULONG_PTR)HKEY_LOCAL_MACHINE,
+    users = (ULONG_PTR)HKEY_USERS,
+};
 
-	private:
-		shared_handle<hkey> hkey;
-	};
-}
+template <reg_root R>
+class reg_handler
+{
+public:
+    reg_handler() = default;
+
+private:
+    shared_handle<hkey> hkey;
+};
+
+} // namespace mwl::windows
